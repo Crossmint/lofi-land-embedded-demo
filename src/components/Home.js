22c4bab249bf560b672b1b24527ecddb538ea233
Danny Mulvihill <danny@paella.dev>
2023-12-07 16:02:01 -0800

    add ethereum icon and split into compoents folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import PurchaseSummary from "./payments/PurchaseSummary";
 import CardPurchase from "./payments/CardPurchase";
 import WalletConnect from "./wallet/WalletConnect";
+import EthereumIcon from "./icons/EthereumIcon";
+import CreditCardIcon from "./icons/CreditCardIcon";
 
 const Home = () => {
   const [showPurchaseModal, setShowPurchaseModal] = useState(false);
@@ -149,24 +151,7 @@ const Home = () => {
                         style={{ cursor: "pointer" }}
                       >
                         <div className="svg-icon">
-                          <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M4 9C4 8.44772 4.44772 8 5 8H9C9.55228 8 10 8.44772 10 9C10 9.55228 9.55228 10 9 10H5C4.44772 10 4 9.55228 4 9Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M4 3C1.79086 3 0 4.79086 0 7V17C0 19.2091 1.79086 21 4 21H20C22.2091 21 24 19.2091 24 17V7C24 4.79086 22.2091 3 20 3H4ZM20 5H4C2.89543 5 2 5.89543 2 7V14H22V7C22 5.89543 21.1046 5 20 5ZM22 16H2V17C2 18.1046 2.89543 19 4 19H20C21.1046 19 22 18.1046 22 17V16Z"
-                              fill="currentColor"
-                            />
-                          </svg>
+                          <EthereumIcon />
                         </div>
                         <div className="mt-2 payment-option-title">
                           Pay with ETH(Testnet)
@@ -186,24 +171,7 @@ const Home = () => {
                         style={{ cursor: "pointer" }}
                       >
                         <div className="svg-icon">
-                          <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M4 9C4 8.44772 4.44772 8 5 8H9C9.55228 8 10 8.44772 10 9C10 9.55228 9.55228 10 9 10H5C4.44772 10 4 9.55228 4 9Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M4 3C1.79086 3 0 4.79086 0 7V17C0 19.2091 1.79086 21 4 21H20C22.2091 21 24 19.2091 24 17V7C24 4.79086 22.2091 3 20 3H4ZM20 5H4C2.89543 5 2 5.89543 2 7V14H22V7C22 5.89543 21.1046 5 20 5ZM22 16H2V17C2 18.1046 2.89543 19 4 19H20C21.1046 19 22 18.1046 22 17V16Z"
-                              fill="currentColor"
-                            />
-                          </svg>
+                          <CreditCardIcon />
                         </div>
                         <div className="mt-2 payment-option-title">
                           Pay with Card
